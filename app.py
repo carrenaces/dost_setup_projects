d628505ccef87af2433e2d0fa0755522a20de206
carrenaces <127925065+carrenaces@users.noreply.github.com>
2023-03-21 15:10:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+# streamlit_app.py
 import streamlit as st
 import streamlit.secrets as secrets
-import pandas as pd
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
-# Get the URL for the Google Sheet from the secrets file
-gsheets_url = secrets.get_secret("private_gsheets_url")
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
-# Read the Google Sheet into a pandas DataFrame
-df = pd.read_csv(gsheets_url)
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
-# Display the DataFrame
-st.dataframe(df)
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
